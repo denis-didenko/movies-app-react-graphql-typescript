ee74786218328053594e50c6f831d78e881e6295
Denis Didenko <didenko.denis@insigit.com>
2023-02-21 11:58:55 +0200

    add vercel.json

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -3,8 +3,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const client = new ApolloClient({
   // uri: 'http://localhost:5000/graphql',
   uri: 'https://movies-app-react-graphql-typescript.vercel.app/graphql',
+
   cache: new InMemoryCache(),
-  headers: {
-    'Access-Control-Allow-Origin': '*', // this is required for CORS
-  },
 });
