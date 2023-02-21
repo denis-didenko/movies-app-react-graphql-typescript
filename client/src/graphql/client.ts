44f42e40dd1402f22ed59edfb9a852b7995eef6f
Denis Didenko <didenko.denis@insigit.com>
2023-02-21 10:27:01 +0200

    add cors5

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,8 +2,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:5000/graphql',
-    // uri: 'https://movies-app-react-graphql.herokuapp.com/graphql',
+    // uri: 'http://localhost:5000/graphql',
+    uri: 'https://movies-app-react-graphql.herokuapp.com/graphql',
 
     headers: {
       'Access-Control-Allow-Origin': '*',
