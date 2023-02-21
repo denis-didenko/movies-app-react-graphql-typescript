a435bdecc32b0e3b1df40b585d6bc01ac8fb714a
Denis Didenko <didenko.denis@insigit.com>
2023-02-21 11:32:26 +0200

    add origin localhost

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = new ApolloServer({
   csrfPrevention: true,
   cache: 'bounded',
   cors: {
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000'],
     credentials: true,
   },
   dataSources: () => ({
