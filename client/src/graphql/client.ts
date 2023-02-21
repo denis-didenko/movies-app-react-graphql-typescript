99e7314d273fec75d043eb2cd42e3c92c84d33c9
Denis Didenko <didenko.denis@insigit.com>
2023-02-21 11:37:20 +0200

    add origin localhost

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   // uri: 'http://localhost:5000/graphql',
-  uri: 'https://movies-app-react-graphql-typescript.vercel.app/',
+  uri: 'https://movies-app-react-graphql-typescript.vercel.app/graphql',
   cache: new InMemoryCache(),
   headers: {
     'Access-Control-Allow-Origin': '*', // this is required for CORS
