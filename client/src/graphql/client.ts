41fa9b08b5ac958fa8f786e1c50c99ee21c8d55a
Denis Didenko <didenko.denis@insigit.com>
2023-02-21 12:29:02 +0200

    add vercel

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   // uri: 'http://localhost:5000/graphql',
-  uri: 'https://movies-app-react-graphql-typescript.vercel.app/graphql',
+  uri: 'https://movies-app-react-graphql-typescript-m2ztdxfmd-denis-didenko.vercel.app/graphql',
 
   cache: new InMemoryCache(),
 });
